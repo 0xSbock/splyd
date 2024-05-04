536c1e1726ee5539d4bff253bdafbb8627aa307d
sbock <mail@sbock.dev>
2024-05-04 12:25:56 +1000

    add name change functionality

--- src/UserList.tsx
+++ src/UserList.tsx
@@ -5,9 +5,12 @@ import { Button } from 'primereact/button'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
 import { Dialog } from 'primereact/dialog'
+import { FloatLabel } from 'primereact/floatlabel'
+import { InputText } from 'primereact/inputtext'
 
-import TransactionDoc, { User } from './transactionDoc'
+import TransactionDoc, { User, Id } from './transactionDoc'
 import { DocUrlContext } from './context'
+import { usernameTaken } from './utils'
 
 const header = (
   <div className="flex flex-wrap align-items-center justify-content-between gap-2">
@@ -20,9 +23,10 @@ const createdAt = (user: User) => user.createdAt.toDateString()
 const UserList = () => {
   const docUrl = useContext(DocUrlContext)
   const [doc, changeDoc] = useDocument<TransactionDoc>(docUrl)
-  const [visibleEditDialog, setVisibleEditDialog] = useState(false)
+  const [toEditID, setToEditID] = useState<Id | undefined>(undefined)
+  const [newUsername, setNewUsername] = useState('')
 
-  const handleUserDelete = (id: string) => {
+  const handleUserDelete = (id: Id) => {
     const userIndex = doc?.users.findIndex((user: User) => user.id === id)
     if (userIndex === -1) {
       // TODO: handle error case
@@ -30,6 +34,20 @@ const UserList = () => {
       return
     }
     changeDoc((d) => A.deleteAt(d.users, userIndex as number))
+  }
+
+  const handleEditSave = (id: Id | undefined) => {
+    if (!id) return
+    if (usernameTaken(doc, newUsername)) {
+      console.info('username already taken. thus, cannot rename.')
+      return
+    }
+    changeDoc((d) => {
+      const userIndex = d.users.findIndex((u) => u.id === id)
+      if (userIndex === -1) return
+      d.users[userIndex].name = newUsername
+    })
+    onDialogHide()
   }
 
   const DeleteUser = (user: User) => (
@@ -40,23 +58,43 @@ const UserList = () => {
     />
   )
 
-  const EditUser = () => (
+  const onDialogOpen = (user: User) => {
+    setToEditID(user.id)
+    setNewUsername(user.name)
+  }
+
+  const onDialogHide = () => {
+    setToEditID(undefined)
+    setNewUsername('')
+  }
+
+  const EditUser = (user: User) => (
     <Button
       icon="pi pi-pencil"
       severity="info"
-      onClick={() => setVisibleEditDialog(true)}
+      onClick={() => onDialogOpen(user)}
     />
   )
+
+  const userEditing = doc?.users.find((u: User) => u.id === toEditID)
 
   return (
     <>
       <Dialog
-        header="Header"
-        visible={visibleEditDialog}
+        header={`Edit User: ${userEditing?.name}`}
+        visible={toEditID !== undefined}
         style={{ width: '50vw' }}
-        onHide={() => setVisibleEditDialog(false)}
+        onHide={() => onDialogHide}
       >
-        <h1>Foobar</h1>
+        <FloatLabel>
+          <InputText
+            id="newUsername"
+            value={newUsername}
+            onChange={(e) => setNewUsername(e.target.value)}
+          />
+          <label htmlFor="newUsername">New Username</label>
+        </FloatLabel>
+        <Button onClick={() => handleEditSave(userEditing?.id)}>Save</Button>
       </Dialog>
       <div className="card">
         <DataTable
