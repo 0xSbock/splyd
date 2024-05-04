4c0a5bd5780fa7dc934b907141ac245998491028
sbock <mail@sbock.dev>
2024-05-04 18:10:16 +1000

    prettier formatting

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,10 @@
 import { Doc } from '@automerge/automerge'
 import TransactionDoc from './transactionDoc'
 
-export const usernameTaken = (doc: Doc<TransactionDoc> | undefined, username: string) => {
+export const usernameTaken = (
+  doc: Doc<TransactionDoc> | undefined,
+  username: string
+) => {
   const existingUsernames = doc?.users.map((u) => u.name) || []
   return existingUsernames.findIndex((u) => u === username) !== -1
 }
